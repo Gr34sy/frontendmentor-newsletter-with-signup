f926be9925f12b9153d88f11c5d54438e4aaa9e6
Gr34sy <slupek3@poczta.fm>
2024-03-13 15:23:42 +0100

    added form validation

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,10 +3,40 @@ import signUpDesktop from "../assets/images/illustration-sign-up-desktop.svg";
 import signUpMobile from "../assets/images/illustration-sign-up-mobile.svg";
 import iconList from "../assets/images/icon-list.svg";
 
+//hooks
+import { useState } from "react";
+import { useEffect } from "react";
+
 export function Card({action}) {
-  function handleSubmit(){
+  const [emailInput, setEmailInput] = useState("");
+  const [showValidationMessage, setShowValidationMessage] = useState(false);
+
+  useEffect(() => {
+    if(showValidationMessage){
+      setTimeout(() => {
+        setShowValidationMessage(false);
+      }, 5000);
+    }
+
+    return () => {}
+  }, [showValidationMessage])
+
+  function handleEmailChange(e){
+    setEmailInput(e.target.value);
+  } 
+
+  function handleSubmit(e){
+    e.preventDefault();
+
     if(typeof action === "function"){
-      action();
+
+      const validationTest = emailInput.includes('@') && emailInput.includes('@') !== "" ? true : false;
+
+      if(validationTest){
+        action();
+      }else{
+        setShowValidationMessage(true);
+      }
     }
   }
 
@@ -40,16 +70,17 @@ export function Card({action}) {
           </li>
         </ul>
         
-        <form className="content__form" onSubmit={handleSubmit}>
+        <form className="content__form" noValidate onSubmit={handleSubmit}>
             <label htmlFor="email">
                 <p>
                 Email address
                 </p>
 
-                <p>
-                </p>
+                {showValidationMessage && <p className="content__form_validation-message">
+                  Valid email required
+                </p>}
             </label>
-            <input type="email" id="email" placeholder="email@company.com" autoComplete="off"/>
+            <input type="email" id="email" className={ showValidationMessage ? 'custom-input input--red':"custom-input"} placeholder="email@company.com" autoComplete="off" value={emailInput} onChange={handleEmailChange}/>
             <button type="submit" className="custom-button">Subscribe to monthly newsletter</button>
         </form>
       </div>
